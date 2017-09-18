77f5d12985042edc0d56f5d4e9b96dedd4697b29
Frank Muñoz <Frank Muoz@LAPTOP-O1LTEOHG>
2017-09-17 19:40:04 -0500

    pagina final

---
+++ encuentro.php
@@ -0,0 +1,8 @@
+<?php
+$cachondo=$_REQUEST ["cachondo"];
+$cachonda=$_REQUEST ["cachonda"];
+echo $cachondo;
+echo "se encuentra en el motel para hacer cosas ricas con";
+echo $cachonda;
+?>
+
---
+++ index.php
@@ -0,0 +1,18 @@
+
+<html>
+    <head>
+        
+        <title>
+            Motel
+        </title>
+    </head>
+    <body>
+        <form action ="encuentro.php">
+            Cachondo
+            <input type="text" name="cachondo">
+            Cachonda
+            <input type="text" name="cachonda">
+            <input type="submit" name="encuentro">
+        </form>
+    </body>
+</html>
